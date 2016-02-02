a8f57b6b6009844d22089c863243cbe5d71bccbf
Joe Sullivan <josulliv101@gmail.com>
2016-02-02 13:47:57 -0500

    dotenv

--- scripts/server.js
+++ scripts/server.js
@@ -1,5 +1,3 @@
-
-
 import express from "express"
   
 var app = express();
@@ -7,7 +5,6 @@ var app = express();
 app.get("/", (req, res) => {
     res.json({hello: 'world'});
 });
- 
  
 var port = process.env.PORT || 3000;
  
